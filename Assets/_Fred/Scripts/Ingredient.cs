a427ac44c570d0e5089812cb23b6920978c39123
FritzEduard <frederic.gaba@gmail.com>
2021-05-25 07:02:35 +0000

    Food cooking animation fix

--- Assets/_Fred/Scripts/Ingredient.cs
+++ Assets/_Fred/Scripts/Ingredient.cs
@@ -6,24 +6,18 @@ public class Ingredient : MonoBehaviour
     public SO_Ingredient IngredientSO;
     public Rigidbody Rigidbody;
 
-    [Header("Settings")]
-    [SerializeField] private float miniJumpDelay; //Minimum delay before making the ingredient jump for his cooking "animation"
-    [SerializeField] private float maxJumpDelay; //Maximum delay before making the ingredient jump for his cooking "animation"
-
     private void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
     }
 
+    [ContextMenu("COOK")]
     public void Cook()
     {
         if (Rigidbody == null) return;
 
-        Invoke("AddImpulse", Random.Range(0.3f,0.5f));
-    }
-
-    private void AddImpulse()
-    {
         Rigidbody.AddForce(Vector3.up, ForceMode.Impulse);
+
+        Invoke("Cook", Random.Range(0.3f,0.5f));
     }
 }
